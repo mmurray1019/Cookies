22d54ab6306f737e78e6db03cccc9cea295d74c8
mmurray1019 <157225528+mmurray1019@users.noreply.github.com>
2024-04-30 12:01:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let cookie = 0;
-
 function addCookie(){
      cookie += 1;
   document.getElementById("cookies").innerHTML = cookie;
@@ -8,6 +7,6 @@ function save(){
   localStorage.setItem("cookies", cookie);
 };
 function load(){
-let cookie = Number(localStorage.cookies);
+cookie = Number(localStorage.cookies);
 document.getElementById("cookies").innerHTML = cookie;
 };
