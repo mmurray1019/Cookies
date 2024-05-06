f6f5b0bae786d63213ae5a53f0ce628667079734
mmurray1019 <157225528+mmurray1019@users.noreply.github.com>
2024-05-06 14:27:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
-let cookies = 0;
-function addCookie(){
-     cookies += 1;
-  document.getElementById("cookies").innerHTML = cookies;
-};  
+var cookies = 0;
+var cursors = 0;
 function save(){
   var save = {
-       cookies:cookies
+       cookies:cookies,
+       cursors:cursors
   }
      localStorage.setItem("save",JSON.stringify(save));
 };
 function load(){
 var savegame = JSON.parse(localStorage.getItem("save"));
      if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
-document.getElementById("cookies").innerHTML = cookies;
+document.getElementById("cookies").innerHTML = prettify(cookies);
+if (typeof savegame.cursors !== "undefined") cursors = savegame.cursors;
+document.getElementById("cursors").innerHTML = prettify(cursors);
+//if (typeof savegame.cursorCost !== "undefined") cursorCost = savegame.cursorCost;
+//document.getElementById("cursorCost").innerHTML = cursorCost;
+};
+
+function prettify(input){
+    var output = Math.round(input * 1000000)/1000000;
+	return output;
+}
+
+
+
+function cookieClick(number){
+    cookies = cookies + number;
+    document.getElementById("cookies").innerHTML = prettify(cookies);
 };
 
 
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+    	cookies = cookies - cursorCost;                          //removes the cookies spent
+        document.getElementById('cursors').innerHTML = prettify(cursors);  //updates the number of cursors for the user
+        document.getElementById('cookies').innerHTML = prettify(cookies);  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cursorCost').innerHTML = prettify(nextCost);  //updates the cursor cost for the user
+};
+
+window.setInterval(function(){
+	
+	cookieClick(cursors);
+	
+}, 1000);
+
+
+
+function deleteion(){
+    localStorage.removeItem("save");
+}
